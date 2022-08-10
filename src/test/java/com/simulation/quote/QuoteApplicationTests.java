36c024f99bba3755bec81b18016c720aafdecbde
brunoBMonteiro <brunobilheri@hotmail.com>
2022-08-10 17:01:36 -0300

    Quote simulation project

---
+++ src/test/java/com/simulation/quote/QuoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulation.quote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
